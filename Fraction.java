e42a794c63b771148d4d019ed495ccf429844e2c
1858 <nox308421@gmail.com>
2024-01-29 15:16:08 +0100

    bis Abschnitt 6 & multiplikation & add() fix

--- Fraction.java
+++ Fraction.java
@@ -1,35 +1,76 @@
 public class Fraction {
     static int numberOfFractions;
-    int numerator, denominator;
+    private int numerator, denominator;
+
+    //Constructors:
+    public Fraction(int numerator, int denominator) {
+        this.numerator = numerator;
+        this.setDenominator(denominator);
+    }
     
-    // public Fraction(Fraction f) {
-    //     this(f.numerator, f.denominator);
-    // }
+    public Fraction(Fraction f) {
+        this(f.numerator, f.denominator);
+    }
 
-    // public Fraction() {
-    //     this(0, 1);
-    // }
+    public Fraction() {
+        this(0, 1);
+    }
 
-    // public void setDenominator(int denominator) {
-    //     if (denominator == 0) {
-    //         throw new ArithmeticException("Division by zero");
-    //     } else {
-    //         this.denominator = denominator;
-    //     }
-    // }
+    //setters:
+    public void setNumerator(int numerator) {
+        this.numerator = numerator;
+    }
 
-    // public void add(Fraction f) {
-    //     this.setNumerator(this.numerator * f.denominator + this.denominator * f.numerator);
-    //     this.setDenominator(this.denominator * f.denominator);
-    // }
+    public void setDenominator(int denominator) {
+        if (denominator == 0) {
+            throw new ArithmeticException("Division by zero");
+        } else {
+            this.denominator = denominator;
+        }
+    }
 
-    // public static Fraction add(Fraction f, Fraction g) {
-    //     f.add(g);
-    //     return f;
-    // }
+    //getters:
+    public int getNumerator() {
+        return numerator;
+    }
+
+    public int getDenominator() {
+        return denominator;
+    }
+
+    //toString:
+    @Override
+    public String toString() {
+        return numerator + "/" + denominator;
+    }
     
-    // public boolean equals(Fraction f) {
-    //     return this.numerator * f.denominator == this.denominator * f.numerator;
-    // }
+    //equals:
+    public boolean equals(Fraction f) {
+        return this.numerator * f.denominator == this.denominator * f.numerator;
+    }
+
+    //addition:
+    public void add(Fraction f) {
+        this.setNumerator(this.numerator * f.denominator + this.denominator * f.numerator);
+        this.setDenominator(this.denominator * f.denominator);
+    }
+
+    public static Fraction add(Fraction f, Fraction g) {
+        Fraction a = new Fraction(f);
+        a.add(g);
+        return a;
+    }
+    
+    //multiplication:
+    public void multiply(Fraction f) {
+        this.setNumerator(this.numerator * f.numerator);
+        this.setDenominator(this.denominator * f.denominator);
+    }
+
+    public static Fraction multiply(Fraction f, Fraction g) {
+        Fraction a = new Fraction(f);
+        a.multiply(g);
+        return a;
+    }
 
 }
--- FractionTester.java
+++ FractionTester.java
@@ -1,19 +1,33 @@
 public class FractionTester {
     public static void main(String[] args) {
         Fraction f = new Fraction();
-        // Der folgende Code funktioniert nur, wenn die Felder von Fraction nicht privat sind:
-        f.numerator = 12;
-        f.denominator = 0;
-        System.out.println(f.numerator + "/" + f.denominator);
-        
+
+        // Der folgende Code funktioniert nur, wenn die Felder von Fraction nicht privat
+        // sind:
+        // f.numerator = 12;
+        // f.denominator = 0;
+        // System.out.println(f.numerator + "/" + f.denominator);
+
+        f.setNumerator(126);
+        f.setDenominator(3);
+
         // toString():
-
+        System.out.println(f);
         // Encapsulation, Getter und Setter:
-
+        Fraction g = new Fraction(16, 2);
+        Fraction m = new Fraction(42, 1);
         // Copy-Konstruktor und equals():
-        
+        Fraction h = new Fraction(f);
+        System.out.println(m + " und " + f + " sind " + (m.equals(f) ? "" : "nicht ") + "gleich!");
+        System.out.println(m + " und " + g + " sind " + (m.equals(g) ? "" : "nicht ") + "gleich!");
         // Klassen- und Instanzenmethoden:
-
+        Fraction a = new Fraction(1,2);
+        Fraction b = new Fraction(2,3);
+        Fraction c = new Fraction(1,5);
+        Fraction d = Fraction.add(a,b);
+        System.out.println(d+" "+a+" "+b);
+        a.add(c);
+        System.out.println(a);
         // statische Variablen:
 
     }
