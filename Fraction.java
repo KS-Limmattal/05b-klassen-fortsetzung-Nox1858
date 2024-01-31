b94b6ffd983340a7c98dfb9a71c2f5203df6095e
1858 <nox308421@gmail.com>
2024-01-31 14:40:20 +0100

    Fractions

--- Fraction.java
+++ Fraction.java
@@ -42,12 +42,37 @@ public class Fraction {
     //toString:
     @Override
     public String toString() {
+        simplify();
+        // gemischte Nummern sind unschön...
+        if(numerator/denominator >= 1 || numerator/denominator <= -1){
+            if(numerator%denominator != 0){
+                if(numerator <0){
+                    // wenn der bruch negativ ist, bsp. -3/2, soll es nicht -1+-1/2 geben, sondern -1-1/2
+                    return ""+numerator/denominator+numerator%denominator+"/"+denominator;
+                } else{
+                    return ""+numerator/denominator+"+"+numerator%denominator+"/"+denominator;
+                }
+            } else{
+                return ""+numerator/denominator;
+            }
+        }
         return numerator + "/" + denominator;
     }
-    
+
     //equals:
     public boolean equals(Fraction f) {
         return this.numerator * f.denominator == this.denominator * f.numerator;
+    }
+    
+    //simplification:
+    public void simplify(){
+        if(this.denominator < 0){
+            this.numerator = -this.numerator;
+            this.denominator = -this.denominator;
+        }
+        int divisor = gcd(this.numerator,this.denominator);
+        this.numerator /= divisor;
+        this.denominator /= divisor;
     }
 
     //addition:
@@ -57,6 +82,16 @@ public class Fraction {
     }
 
     public static Fraction add(Fraction f, Fraction g) {
+        Fraction a = new Fraction(f);
+        a.add(g);
+        return a;
+    }
+
+    public void add(int f) {
+        this.setNumerator(this.numerator + this.denominator * f);
+    }
+
+    public static Fraction add(Fraction f, int g) {
         Fraction a = new Fraction(f);
         a.add(g);
         return a;
@@ -74,4 +109,86 @@ public class Fraction {
         return a;
     }
 
+    public void multiply(int f) {
+        this.setNumerator(this.numerator * f);
+    }
+
+    public static Fraction multiply(Fraction f, int g) {
+        Fraction a = new Fraction(f);
+        a.multiply(g);
+        return a;
+    }
+
+    //subtraction:
+    public void subtract(Fraction f) {
+        Fraction a = new Fraction(f);
+        a.setNumerator(-a.numerator);
+        this.add(a);
+    }
+
+    public static Fraction subtract(Fraction f, Fraction g) {
+        Fraction a = new Fraction(f);
+        Fraction b = new Fraction(g);
+        b.setNumerator(-b.numerator);
+        a.add(b);
+        return a;
+    }
+
+    public void subtract(int a) {
+        this.add(-a);
+    }
+
+    public static Fraction subtract(Fraction f, int g) {
+        Fraction a = new Fraction(f);
+        a.add(-g);
+        return a;
+    }
+    
+    //dividation:
+    public void divide(Fraction f) {
+        if(f.numerator == 0){
+            throw new ArithmeticException("Division by zero");
+        }
+        this.setNumerator(this.numerator * f.denominator);
+        this.setDenominator(this.denominator * f.numerator);
+    }
+
+    public static Fraction divide(Fraction f, Fraction g) {
+        if(g.numerator == 0){
+            throw new ArithmeticException("Division by zero");
+        }
+        Fraction a = new Fraction(f);
+        Fraction b = new Fraction(g);
+        int save = b.numerator;
+        b.setNumerator(b.denominator);
+        b.setDenominator(save);
+        a.multiply(b);
+        return a;
+    }
+
+    public void divide(int f) {
+        if(f == 0){
+            throw new ArithmeticException("Division by zero");
+        }
+        this.setDenominator(this.denominator * f);
+    }
+
+    public static Fraction divide(Fraction f, int g) {
+        if(g == 0){
+            throw new ArithmeticException("Division by zero");
+        }
+        Fraction a = new Fraction(f);
+        Fraction b = new Fraction(1,g);
+        a.multiply(b);
+        return a;
+    }
+
+    // copypaste von Aufgabe 15 c) Rekursion
+    private static int gcd(int a, int b){
+        if(b%a != 0){
+            return gcd(b%a,a);
+        } else{
+            return a;
+        }
+    }
 }
--- FractionTester.java
+++ FractionTester.java
@@ -1,5 +1,6 @@
 public class FractionTester {
     public static void main(String[] args) {
+        final double PI = 3.14159;
         Fraction f = new Fraction();
 
         // Der folgende Code funktioniert nur, wenn die Felder von Fraction nicht privat
@@ -30,6 +31,75 @@ public class FractionTester {
         System.out.println(a);
         // statische Variablen:
         System.out.println(Fraction.numberOfFractions);
+        // Konstanten:
+        // PI++; //geht nicht, wird von VSC unterstrichen
+        System.out.println(PI);
+
+         /* public static void main(String args){}
+         * public static, da von auserhalb darauf zugegriffen werden soll (Programm ausführen zum Beispiel) und änderungen vorgenommen werden sollen. 
+         * void, da es bloss ausführt und nichts returnen soll.
+         * main, der Name der Methode, so gewählt das Java versteht das diese Methode das ist was ausgeführt werden soll.
+         * String args, die Variablen, die beim Aufrufen der Klasse eingegeben werden, darauf kann vom Code aus zugegriffen werden.
+         * 
+         * Sichtbarkeit:
+         * Ich kann nichts unterstreichen, also schreibe ich (static) vor die 'unterstrichenen' Methoden
+         * UML:
+         * 
+         * Fraction.java
+         * + numberOfFractions : int
+         * - numerator: int
+         * - denominator: int
+         * ____________________________
+         * + Fraction()
+         * + Fraction(f : Fraction)
+         * + Fraction(int : numerator, int : denominator)
+         * + setNumerator(int: numerator)
+         * + setDenominator(int : denominator)
+         * + getNumerator : int
+         * + getDenominator : int
+         * + toString() : String
+         * + equals() : bool
+         * + add(Fraction f)
+         * + (static) add(Fraction f, Fraction g) : Fraction
+         * + multiply(Fraction f)
+         * + (static) multiply(Fraction f, Fraction g) : Fraction
+         * 
+         * (als nächstes folgt noch:)
+         * 
+         * + subtract(Fraction f)
+         * + (static) subtract(Fraction f, Fraction g) : Fraction
+         * + divide(Fraction f)
+         * + (static) divide(Fraction f, Fraction g) : Fraction
+         * 
+         * + simplify()
+         * 
+         * + add(int a)
+         * + (static) add(Fraction f, int a) : Fraction
+         * + multiply(int a)
+         * + (static) multiply(Fraction f, int a) : Fraction
+         * + subtract(int a)
+         * + (static) subtract(Fraction f, int a) : Fraction
+         * + divide(int a)
+         * + (static) divide(Fraction f, int a) : Fraction
+         */
+
+        //Testcode für simplify()
+        Fraction l = new Fraction(6, 9);
+        Fraction n = new Fraction(125,25);
+        System.out.println(l+" "+n);
+        Fraction.divide(l, 2);
+        l.divide(n);
+        System.out.println(l);
+        try{
+            n.divide(0);
+        } catch(Exception e){
+            System.out.println(e);
+        }
+        n.subtract(l);
+        System.out.println(n);
+        Fraction o = new Fraction(-3,12);
+        n.divide(o);
+        System.out.println(n);
     }
 
 }
